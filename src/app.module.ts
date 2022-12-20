0f67c9ca7dab4345956c1408b6844ea2c56dc065
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-12-20 19:41:40 +0900

    app.module 파일 생성 git env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
-
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [],
+      synchrocize: true,
+    }),
+  ],
 })
 export class AppModule {}
