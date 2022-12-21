feae3e8f46739056342f5a6983f69a55a6e23ca7
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-12-21 19:18:57 +0900

    매롱

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
+
 import * as dotenv from 'dotenv';
 dotenv.config();
+// type: process.env.TYPEORM_CONNECTION,
 
 @Module({
   imports: [
+    UserModule,
     TypeOrmModule.forRoot({
-      type: process.env.TYPEORM_CONNECTION,
+      type: 'mysql',
       host: process.env.TYPEORM_HOST,
-      port: process.env.TYPEORM_PORT,
+      port: +process.env.TYPEORM_PORT,
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
       entities: [],
-      synchrocize: true,
+      synchronize: true,
+      autoLoadEntities: true,
+      retryAttempts: 5,
     }),
   ],
+  controllers: [UserController],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private datasource: DataSource) {}
+}
