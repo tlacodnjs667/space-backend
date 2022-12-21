6d4f678d277096deb1bef5bf83fcd7d3af763776
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-12-21 21:57:45 +0900

    데이터베이스 연결 성공 아마두...
    wip
    
    wip
    
    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { UserModule } from './user/user.module';
-import { UserController } from './user/user.controller';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -10,22 +7,17 @@ dotenv.config();
 
 @Module({
   imports: [
-    UserModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: process.env.TYPEORM_CONNECTION,
       host: process.env.TYPEORM_HOST,
-      port: +process.env.TYPEORM_PORT,
+      port: process.env.TYPEORM_PORT,
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
       entities: [],
-      synchronize: true,
-      autoLoadEntities: true,
-      retryAttempts: 5,
+      synchrocize: false,
     }),
   ],
-  controllers: [UserController],
 })
-export class AppModule {
-  constructor(private datasource: DataSource) {}
-}
+
+export class AppModule { }
---
+++ src/config/configuration.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+export default () => ({
+  database: {
+    type: process.env.TYPEORM_CONNECTION || '', 
+    host: process.env.TYPEORM_HOST || '',
+    port: process.env.TYPEORM_PORT || '',
+    username: process.env.TYPEORM_USERNAME || '',
+    password: process.env.TYPEORM_PASSWORD || '',
+    database: process.env.TYPEORM_DATABASE || '',
+  },
+});
