7d69f775c26c293f4ff44a955da1fc99e97e7558
SHIN INHUCK <sih9492@gmail.com>
2022-12-23 15:34:54 +0900

    wip

---
+++ src/entities/calendar_like.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { LaunchingCalendar } from './launching_calendar.entity';
+import { User } from './user.entity'
+@Entity()
+export class CalendarLike {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @ManyToOne(() => User, (user) => user.id, { nullable: true, cascade: false })
+    user_id: User;
+    @ManyToOne(() => LaunchingCalendar, (launching_calendar) => launching_calendar.id, { nullable: true, cascade: false })
+    launching_calendar: User;
+}
+
---
+++ src/entities/event_winner_post.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } from 'typeorm';
+import { Admin } from './admin.entity';
+@Entity()
+export class CalendarComment {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @Column({ length: 100 })
+    title: string;
+    @Column({ length: 2000 })
+    description: string;
+    @CreateDateColumn()
+    created_at: Date;
+    @Column('int')
+    seeing_count: number;
+    @ManyToOne(() => Admin, (admin) => admin.id, { nullable: true, cascade: false })
+    admin_id: Admin;
+}
