e393260f08e21097e917204afe4994e372aef922
SHIN INHUCK <sih9492@gmail.com>
2023-01-09 14:16:44 +0900

    wip

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+// import { CreateProductDto } from './dto/create-product.dto';
+// import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductService {
