b1665511f219352b2d985c19042cfbdbf6aa9b95
SHIN INHUCK <sih9492@gmail.com>
2023-01-17 13:48:38 +0900

    [ADD] lookbookDetail page

--- src/API/lookbook/lookbook.service.ts
+++ src/API/lookbook/lookbook.service.ts
@@ -6,4 +6,7 @@ export class LookbookService {
   getLookbookList(offset: number) {
     return LookbookRepository.getLookbookList(offset);
   }
+  getLookbookDetail(lookbookId: string) {
+    return LookbookRepository.getLookbookDetail(lookbookId);
+  }
 }
