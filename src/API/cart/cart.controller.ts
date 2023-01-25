97b0cebf799ddc8eef69b358c4fbec68ec1e9fc1
maison <110589528+ShinInHuck@users.noreply.github.com>
2023-01-25 18:14:40 +0900

    [ADD] cart CRUD (#52)

--- src/API/cart/cart.controller.ts
+++ src/API/cart/cart.controller.ts
@@ -1,13 +1,57 @@
-import { Controller, Post, Body, Headers } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Patch,
+  Query,
+  Headers,
+  Delete,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
-import { CreateCartDto } from './dto/create-cart.dto';
+import {
+  CreateCartDto,
+  UpdateCartDto,
+  UpdateItemDto,
+} from './dto/create-cart.dto';
 
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Post()
-  async createUserCart(@Body() req: CreateCartDto) {
-    return this.cartService.createUserCart(req);
+  async createUserCart(
+    @Body() cartItem: CreateCartDto,
+    @Headers('user') userId: number,
+  ) {
+    return this.cartService.createUserCart(cartItem, +userId);
+  }
+
+  @Get()
+  getUserCart(@Headers('user') userId: number) {
+    return this.cartService.getUserCart(userId);
+  }
+
+  @Patch('quantity')
+  updateQuantityCart(
+    @Query() optionItem: UpdateCartDto,
+    @Headers('user') userId: number,
+  ) {
+    return this.cartService.updateQuantityCart(optionItem, userId);
+  }
+
+  @Patch('option')
+  updateProdutCart(
+    @Query() CartOption: UpdateItemDto,
+    @Headers('user') userId: number,
+  ) {
+    return this.cartService.updateProductCart(CartOption, userId);
+  }
+  @Delete()
+  deleteCart(
+    @Headers('user') userId: number,
+    @Query('cartId') cartId: number | number[],
+  ) {
+    return this.cartService.deleteCart(userId, cartId);
   }
 }
