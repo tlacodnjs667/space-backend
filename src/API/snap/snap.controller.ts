1d336d0971d4bbfb191da5e201162e15e2a90445
maison <110589528+ShinInHuck@users.noreply.github.com>
2023-01-25 16:18:50 +0900

    [ADD] snapList && snapDetailPage (#50)

--- src/API/snap/snap.controller.ts
+++ src/API/snap/snap.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { SnapService } from './snap.service';
-
+import { SnapDto } from './dto/create-snap.dto';
 @Controller('snap')
 export class SnapController {
   constructor(private readonly snapService: SnapService) {}
-  @Get('main')
-  getSnapsForAdv() {
-    return this.snapService.getSnapsForAdv();
+
+  @Get()
+  getSnapList(@Query('hashtag') hashtag: string) {
+    return this.snapService.getSnapList(hashtag);
+  }
+  @Get('detail')
+  getSnapDetail(@Query() snap: SnapDto) {
+    return this.snapService.getSnapDetail(snap);
   }
 }
