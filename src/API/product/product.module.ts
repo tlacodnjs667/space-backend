09e7ae51c85a448d7b3b34d96cd88e53db8ee9ae
SHIN INHUCK <sih9492@gmail.com>
2023-01-27 21:43:40 +0900

    [FIX] productWishlistPage

--- src/API/product/product.module.ts
+++ src/API/product/product.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
-
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
